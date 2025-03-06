2f50bb4a7d5b47cd3adea8c49dc81b7ed131eae4
Kaido Kert <kaidokert@gmail.com>
2025-03-05 20:50:37 -0800

    Async connect saved network

--- feather/examples/async_connect_saved.rs
+++ feather/examples/async_connect_saved.rs
@@ -2,24 +2,28 @@
 #![no_main]
 
 use cortex_m_systick_countdown::MillisCountDown;
-use embassy_time::Timer;
-use feather as bsp;
 use feather::hal::ehal::digital::OutputPin;
 use feather::init2::init;
 use feather::shared::delay_fn;
+use feather::shared::SpiStream;
+use wincwifi::AsyncClient;
+use wincwifi::StackError;
 
 #[cfg(feature = "custom_driver")]
 mod time_driver;
 
-async fn program() -> Result<(), u8> {
+async fn program() -> Result<(), StackError> {
     if let Ok(ini) = init() {
         defmt::info!("Embassy async blinky");
         let mut red_led = ini.red_led;
         let mut cnt = MillisCountDown::new(&ini.delay_tick);
         let mut delay = delay_fn(&mut cnt);
-
-        // TODO: Async-initialize the module
-        // TODO: async-connect to saved network
+        let mut module = AsyncClient::new(SpiStream::new(ini.cs, ini.spi));
+        defmt::info!("Initializing module");
+        module.start_wifi_module().await?;
+        defmt::info!("Connecting to saved network");
+        module.connect_to_saved_ap().await?;
+        defmt::info!("Connected to saved network");
         loop {
             delay(200u32); // Todo: replace this with embbassy_time::Timer::after_millis(200).await
             red_led.set_high().unwrap();
