751aa9a786eba7c9f5dd74dcc0d197a33db0b120
Kaido Kert <kaidokert@gmail.com>
2025-03-09 19:47:13 -0700

    Fix up DNS implementation

--- winc-rs/src/client/dns.rs
+++ winc-rs/src/client/dns.rs
@@ -1,9 +1,9 @@
 use core::net::IpAddr;
+use core::net::Ipv4Addr;
+use embedded_nal::nb;
 use embedded_nal::AddrType;
 use embedded_nal::Dns;
 
-use super::GenResult;
-use super::GlobalOp;
 use super::StackError;
 use crate::transfer::Xfer;
 use crate::WincClient;
@@ -16,21 +16,30 @@ impl<X: Xfer> Dns for WincClient<'_, X> {
         hostname: &str,
         addr_type: AddrType,
     ) -> embedded_nal::nb::Result<IpAddr, Self::Error> {
-        if addr_type != AddrType::IPv4 {
-            unimplemented!("IPv6 not supported");
+        match &mut self.callbacks.dns_resolved_addr {
+            None => {
+                if addr_type != AddrType::IPv4 {
+                    unimplemented!("IPv6 not supported");
+                }
+                self.dispatch_events()?;
+                self.manager
+                    .send_gethostbyname(hostname)
+                    .map_err(StackError::WincWifiFail)?;
+                // Signal operation in progress
+                self.callbacks.dns_resolved_addr = Some(None);
+            }
+            Some(result) => {
+                if let Some(ip) = result.take() {
+                    self.callbacks.dns_resolved_addr = None;
+                    if ip == Ipv4Addr::new(0, 0, 0, 0) {
+                        return Err(StackError::DnsFailed.into());
+                    }
+                    return Ok(IpAddr::V4(ip));
+                }
+            }
         }
-        // Todo: this global_op isn't necessary at all, just use `last_recv_addr`
-        // for passing state back from the callback
-        self.callbacks.global_op = Some(GlobalOp::GetHostByName);
-        self.manager
-            .send_gethostbyname(hostname)
-            .map_err(|_x| StackError::GlobalOpFailed)?;
-        let res = self.wait_for_gen_ack(GlobalOp::GetHostByName, Self::DNS_TIMEOUT)?;
-
-        if let GenResult::Ip(ip) = res {
-            return Ok(IpAddr::V4(ip));
-        }
-        Err(StackError::DnsFailed.into())
+        self.dispatch_events()?;
+        Err(nb::Error::WouldBlock)
     }
 
     fn get_host_by_address(
