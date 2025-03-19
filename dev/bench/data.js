ae5aac65823146dafe76c9f0c02deec557506763
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 04:33:50 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6bdeedc7c3fa01cd59b8db1c9dce12da900a7464

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1742358830627,
+  "repoUrl": "https://github.com/kaidokert/winc-rs",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kaidokert@gmail.com",
+            "name": "Kaido Kert",
+            "username": "kaidokert"
+          },
+          "committer": {
+            "email": "kaidokert@gmail.com",
+            "name": "Kaido Kert",
+            "username": "kaidokert"
+          },
+          "distinct": true,
+          "id": "6bdeedc7c3fa01cd59b8db1c9dce12da900a7464",
+          "message": "Enable push",
+          "timestamp": "2025-03-18T21:33:17-07:00",
+          "tree_id": "448ac403cf34d433aef6f057d8187098c01641bc",
+          "url": "https://github.com/kaidokert/winc-rs/commit/6bdeedc7c3fa01cd59b8db1c9dce12da900a7464"
+        },
+        "date": 1742358830247,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "custommetric",
+            "value": 20,
+            "unit": "kilobyte"
+          }
+        ]
+      }
+    ]
+  }
+}
