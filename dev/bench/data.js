75c7517c779c35e13c984462495fc946d8a0e484
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 05:56:46 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 746e3ac034a53efdfdca40833a94cb5fdaa32607

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742363434927,
+  "lastUpdate": 1742363806741,
   "repoUrl": "https://github.com/kaidokert/winc-rs",
   "entries": {
     "Benchmark": [
@@ -53,6 +53,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kaidokert/winc-rs/commit/5383d7f912c820bfbb32616f5a22bf7bdafb2db1"
         },
         "date": 1742363432965,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "http_server",
+            "value": 40872,
+            "unit": "byte"
+          },
+          {
+            "name": "iperf3_client",
+            "value": 55472,
+            "unit": "byte"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kaidokert@gmail.com",
+            "name": "Kaido Kert",
+            "username": "kaidokert"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "746e3ac034a53efdfdca40833a94cb5fdaa32607",
+          "message": "Make benchmark workflow track binary size\n\nSets up binary size tracking\n\nAlso change --release config to optimize for size",
+          "timestamp": "2025-03-18T22:55:46-07:00",
+          "tree_id": "8057ebfbff11fae7552aa3e9ab08e0f7cbbf24ab",
+          "url": "https://github.com/kaidokert/winc-rs/commit/746e3ac034a53efdfdca40833a94cb5fdaa32607"
+        },
+        "date": 1742363806306,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
