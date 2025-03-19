2664a45bb1edb7180c159be210cb65ec609dbded
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 04:39:43 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6bf28a6d8eb1cc014119c7e040261e308312c568

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742358830627,
+  "lastUpdate": 1742359183431,
   "repoUrl": "https://github.com/kaidokert/winc-rs",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kaidokert/winc-rs/commit/6bdeedc7c3fa01cd59b8db1c9dce12da900a7464"
         },
         "date": 1742358830247,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "custommetric",
+            "value": 20,
+            "unit": "kilobyte"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kaidokert@gmail.com",
+            "name": "Kaido Kert",
+            "username": "kaidokert"
+          },
+          "committer": {
+            "email": "kaidokert@gmail.com",
+            "name": "Kaido Kert",
+            "username": "kaidokert"
+          },
+          "distinct": true,
+          "id": "6bf28a6d8eb1cc014119c7e040261e308312c568",
+          "message": "Enable push",
+          "timestamp": "2025-03-18T21:38:58-07:00",
+          "tree_id": "448ac403cf34d433aef6f057d8187098c01641bc",
+          "url": "https://github.com/kaidokert/winc-rs/commit/6bf28a6d8eb1cc014119c7e040261e308312c568"
+        },
+        "date": 1742359182604,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
