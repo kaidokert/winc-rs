47934b901e866f07728296ad449930025a0c61a0
Kaido Kert <kaidokert@gmail.com>
2025-06-26 21:02:18 -0700

    Cleanups

--- winc-rs/src/lib.rs
+++ winc-rs/src/lib.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Dual logging system compatibility: defmt doesn't support modern format syntax
+#![allow(clippy::uninlined_format_args)]
+
 //! ATWINC1500 Wifi module library
 //!
 //! NOTE: This very much Work In Progress
